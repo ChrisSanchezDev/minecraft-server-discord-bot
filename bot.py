c3e4f31e4fab0ccda563f60c097642282a04f8af
ChrisSanchezDev <ChrisSanchezDev@gmail.com>
2026-01-15 16:44:08 -0500

    Add Stop Server button functionality with magic packet support

--- bot.py
+++ bot.py
@@ -266,6 +266,28 @@ class ServerControlView(discord.ui.View):
     async def refresh_button(self, interaction: discord.Interaction):
         await interaction.response.defer(ephemeral=True)
         await update_server_info()
+    
+    ''' Stop Server button for when no one is on the server (Maybe we can have it so if you have a specific role, you can turn it off at anytime)
+    @discord.ui.button(label='Stop Server', style=discord.ButtonStyle.red, custom_id='stop_btn', emoji='⛔')
+    async def stop_button(self, interaction: discord.Interaction, button: discord.ui.Button):
+        # Gives a defer message to discord (it asks for Discord to wait longer for a response)
+        await interaction.response.defer(ephemeral=True)
+
+        if display_status != 'online':
+            button.disabled = True
+
+        send_magic_packet(MSI_MAC) # ??? If this doesn't work, will it crash or just continue?
+
+        await interaction.followup.send(
+            'Magic Packet sent! The server is waking up. The dashboard will update shortly.',
+            ephemeral=True
+        )
+
+        embed = create_status_embed(display_status='booting')
+
+        button.disabled = True # ??? Does this gray out the button?
+        await interaction.message.edit(embed=embed, view=self)
+    '''
 
 # -----BACKGROUND LOOPS & EVENTS-----
 @tasks.loop(seconds=30)
